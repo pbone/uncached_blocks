4259811367902b3459737f01a6229c4f070d091b
Patrick Hall <pbone3b@gmail.com>
2026-06-12 12:02:47 -0400

    Add test block plugin for testing uncached blocks functionality

---
+++ src/Plugin/Block/TestUncachedBlock.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Drupal\uncached_blocks\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Block\Attribute\Block;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+
+#[Block(
+  id: 'test_uncached_block',
+  admin_label: new TranslatableMarkup('Test Uncached Block'),
+)]
+class TestUncachedBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    return [
+      '#markup' => 'Block rendered at: ' . date('Y-m-d H:i:s'),
+    ];
+  }
+
+}
